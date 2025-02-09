c481d35141d8bdfcfad52059a49fc43d919bb5db
Venkat2277 <112927607+Venkat2277@users.noreply.github.com>
2025-02-09 12:33:35 -0500

    Added server, Axios, and ChartJS

--- .history/server_20250208121733.js
+++ .history/server_20250208121733.js
@@ -5,25 +5,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//app.use(cors());
 
-app.use('/', express.static('public'));
-
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
+//const budget = {
+    //myBudget: [
+        //{
+            //title: 'Eat out',
+            //budget: 25
+        //},
+        //{
+            //title: 'Rent',
+            //budget: 275
+        //},
+        //{
+            //title: 'Grocery',
+            //budget: 110
+        //},
+    //]
+//};
 
 
 //app.get('/budget', (req, res) => {
@@ -33,12 +32,8 @@ const budget = {
 //app.listen(port, () => {
     //console.log(`API served at http://localhost:${port}`);
 //});
-app.get('/hello', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello World!');
-});
-
-app.get('/budget', (req, res) => {
-    res.json(budget);
 });
 
 app.listen(port, () => {
--- .history/server_20250208122853.js
+++ .history/server_20250208122853.js
@@ -8,22 +8,22 @@ const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
+//const budget = {
+    //myBudget: [
+        //{
+            //title: 'Eat out',
+            //budget: 25
+        //},
+        //{
+            //title: 'Rent',
+            //budget: 275
+        //},
+        //{
+            //title: 'Grocery',
+            //budget: 110
+        //},
+    //]
+//};
 
 
 //app.get('/budget', (req, res) => {
@@ -35,10 +35,6 @@ const budget = {
 //});
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
-});
-
-app.get('/budget', (req, res) => {
-    res.json(budget);
 });
 
 app.listen(port, () => {
